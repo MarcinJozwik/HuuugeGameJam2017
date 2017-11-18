495ed617f7c7e34ba98ffd22c3c2612011b3086f
Mateusz <189731@edu.p.lodz.pl>
2017-11-18 23:58:07 +0100

    Stuff

---
+++ Assets/HeroDescritionLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HeroDescritionLoader : MonoBehaviour {
+
+    public Text text;
+
+    public void SetHeroDescription(string heroDesc)
+    {
+        text.text = heroDesc;
+    }
+	
+}
