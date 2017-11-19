0142460345ef9b331f46da9326d25cc6ab009e87
MarcinJozwik <marcinjozwik@outllok.com>
2017-11-19 10:19:07 +0100

    Added description generator

---
+++ Assets/Scripts/DescriptionGenerator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class DescriptionGenerator
+{
+    private List<string> heroParams;
+    private readonly List<string> baseFirstWearingVerbs;
+    private readonly List<string> firstWearingVerbs;
+    private readonly List<string> baseSecondWearingVerbs;
+    private readonly List<string> secondWearingVerbs;
+    private string firstItem;
+    private string secondItem;
+
+    public DescriptionGenerator()
+    {
+        firstWearingVerbs = new List<string>();
+        secondWearingVerbs = new List<string>();
+        baseFirstWearingVerbs = new List<string>()
+        {
+            "odziany w",
+            "posiadający",
+            "opasany w",
+            "obtykany",
+            "układły przez",
+            "ustrojony w",
+        };
+        baseSecondWearingVerbs = new List<string>()
+        {
+            "Wdłożyłbym mu także",
+            "Całość uchopia",
+            "Od ogółu odrzeza się",
+            "Wskładałbym do tego",
+            "Warte wględowania się jest też",
+            "Niech wdirży twoją uwagę lepaknię",
+        };
+    }
+
+    public string GetDescription(Hero hero)
+    {
+        heroParams = hero.GetDescription().ToList();
+        string intro = "Mistrzu, w potrzebiźnie przyjacham!\nPotrzebna grumada:\n";
+        ShuffleItemInDescription();
+        return String.Format("{5}{0} niekto {1} {2}. {3} {4}.", CamelString(heroParams[3]), GetRandomFromList(baseFirstWearingVerbs, firstWearingVerbs), firstItem, GetRandomFromList(baseSecondWearingVerbs, secondWearingVerbs), secondItem, intro);
+    }    
+
+    private string GetRandomFromList(List<string> baseList, List<string> currentList)
+    {
+        if (!currentList.Any())
+        {
+            currentList = new List<string>(baseList);
+        }
+
+        int index = Random.Range(0, currentList.Count);
+        string randomString = currentList.ElementAt(index);
+        currentList.RemoveAt(index);
+
+        return randomString;
+    }
+
+    private string CamelString(string heroParam)
+    {
+        return String.Format("{0}{1}", heroParam.Substring(0, 1).ToUpper(), heroParam.Substring(1));
+    }
+
+    private void ShuffleItemInDescription()
+    {
+        if (Random.Range(0, 2) == 0)
+        {
+            firstItem = heroParams[1];
+            secondItem = heroParams[2];
+        }
+        else
+        {
+            secondItem = heroParams[1];
+            firstItem = heroParams[2];
+        }
+    }
+}
