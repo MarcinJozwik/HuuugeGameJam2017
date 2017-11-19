c63b0e2a230097db79101e9df4cb168c5c0b564b
MarcinJozwik <marcinjozwik@outllok.com>
2017-11-19 12:34:46 +0100

    Asset class names

--- Assets/Scripts/Scriptable Objects/HeroParts.cs
+++ Assets/Scripts/Scriptable Objects/HeroParts.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Zmien to kurwa", menuName = "Hero Maker/Hero Parts")]
 public class HeroParts : ScriptableObject
 {
-
     public List<Mesh> Heads;
     public List<Mesh> Torsos;
-
-
+    public List<string> Names;
 }
